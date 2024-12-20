6eca9021cf8dca647c303a9812147a4ddf0ccfe3
sunilneupane27 <167099324+sunilneupane27@users.noreply.github.com>
2024-12-20 17:44:25 +0000

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./App.css"
+import{ useState } from 'react';
+function Hook_ControlledButtonState()
+{
+    const ClickHandle=()=>
+    {
+        setCount(count +1)
+
+    }
+    const [ count, setCount] = useState(0);
+    return (
+        <div className=" App-header">
+        <form>
+            <h1> Click Counts are{count}</h1>
+            <button type= "button" onCLick={ClickHandle}> ClickMe{count}</button> 
+            </form>    
+        </div>
+    );
+}
+export default Hook_ControlledButtonState
